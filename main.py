e5b0a4c45889cd897cdbfb73336a26149983fe02
Johannes Gutekunst <johannes.gutekunst@t-online.de>
2023-03-09 12:27:52 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+import os
+
+import uvicorn
+
+
+@app.get("/")
+async def root():
+    html_content = """
+    <html>
+        <head>
+            <title>Some HTML in here</title>
+        </head>
+        <body>
+            <h1>Fancy Surface Reconstruction (vtk copy paste) 🐦</h1>
+        </body>
+    </html>
+    """
+    return HTMLResponse(content=html_content, status_code=200)
+
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:app", host="0.0.0.0", port=int(os.getenv("PORT", 8000)), log_level="info"
+    )
